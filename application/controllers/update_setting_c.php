4ec15f30bcfc075dd9aa00c76ffdc96e7e363a1a
segarabayuputra <bayuputrasegara@gmail.com>
2016-06-15 16:28:04 +0800

    Add files via upload

--- application/controllers/update_setting_c.php
+++ application/controllers/update_setting_c.php
@@ -21,48 +21,48 @@ class update_setting_c extends CI_controller
 
   function update_data()
   {
-    $waktu_mulai_lomba = $this->input->post['waktu_mulai_lomba'];
-    $data = array('attr_setting' => $waktu_mulai_lomba);
+    $waktu_mulai_lomba = $this->input->post('waktu_mulai_lomba');
+    $data = array('value_setting' => $waktu_mulai_lomba);
     $this->update_setting_m->update($data, 1);
 
-    $waktu_selesai_lomba = $this->input->post['waktu_selesai_lomba'];
-    $data = array('attr_setting' => $waktu_selesai_lomba);
+    $waktu_selesai_lomba = $this->input->post('waktu_selesai_lomba');
+    $data = array('value_setting' => $waktu_selesai_lomba);
     $this->update_setting_m->update($data, 2);
 
-    $banyak_soal_sma = $this->input->post['banyak_soal_sma'];
-    $data = array('attr_setting' => $banyak_soal_sma);
+    $banyak_soal_sma = $this->input->post('banyak_soal_sma');
+    $data = array('value_setting' => $banyak_soal_sma);
     $this->update_setting_m->update($data, 3);
 
-    $banyak_soal_smp = $this->input->post['banyak_soal_smp'];
-    $data = array('attr_setting' => $banyak_soal_smp);
+    $banyak_soal_smp = $this->input->post('banyak_soal_smp');
+    $data = array('value_setting' => $banyak_soal_smp);
     $this->update_setting_m->update($data, 4);
 
-    $banyak_soal_sd = $this->input->post['banyak_soal_sd'];
-    $data = array('attr_setting' => $banyak_soal_sd);
+    $banyak_soal_sd = $this->input->post('banyak_soal_sd');
+    $data = array('value_setting' => $banyak_soal_sd);
     $this->update_setting_m->update($data, 5);
 
-    $waktu_lomba = $this->input->post['waktu_lomba'];
-    $data = array('attr_setting' => $waktu_lomba);
+    $waktu_lomba = $this->input->post('waktu_lomba');
+    $data = array('value_setting' => $waktu_lomba);
     $this->update_setting_m->update($data, 6);
 
-    $header_web = $this->input->post['header_web'];
-    $data = array('attr_setting' => $header_web);
+    $header_web = $this->input->post('header_web');
+    $data = array('value_setting' => $header_web);
     $this->update_setting_m->update($data, 7);
 
-    $tema_web = $this->input->post['tema_web'];
-    $data = array('attr_setting' => $tema_web);
+    $tema_web = $this->input->post('tema_web');
+    $data = array('value_setting' => $tema_web);
     $this->update_setting_m->update($data, 8);
 
-    $nilai_benar = $this->input->post['nilai_benar'];
-    $data = array('attr_setting' => $nilai_benar);
+    $nilai_benar = $this->input->post('nilai_benar');
+    $data = array('value_setting' => $nilai_benar);
     $this->update_setting_m->update($data, 9);
 
-    $nilai_salah = $this->input->post['nilai_salah'];
-    $data = array('attr_setting' => $nilai_salah);
+    $nilai_salah = $this->input->post('nilai_salah');
+    $data = array('value_setting' => $nilai_salah);
     $this->update_setting_m->update($data, 10);
 
-    $nilai_tidak_jawab = $this->input->post['nilai_tidak_jawab'];
-    $data = array('attr_setting' => $nilai_tidak_jawab);
+    $nilai_tidak_jawab = $this->input->post('nilai_tidak_jawab');
+    $data = array('value_setting' => $nilai_tidak_jawab);
     $this->update_setting_m->update($data, 11);
 
     redirect(site_url().'update_setting_c');
